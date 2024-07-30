a80d3aa9b02751516cd7fe2214662372c5960c7e
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-07-30 15:50:25 +0545

    Connected to DB(MongoDB)

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //M  1. MIDDLEWARE
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(morgan('dev'));
 
 app.use(express.json());
+
+/////////////////
+// TO USE STATIC FILE FOLDERS IN OUR APPLICATION
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Jello from the middleware 🫶');
@@ -35,3 +42,13 @@ app.use('/api/v1/users', userRouter);
 // 4. START SERVER
 
 module.exports = app;
+// "devDependencies": {
+//   "eslint": "^8.57.0",
+//   "eslint-config-airbnb": "^19.0.4",
+//   "eslint-config-prettier": "^9.1.0",
+//   "eslint-plugin-import": "^2.29.1",
+//   "eslint-plugin-jsx-a11y": "^6.9.0",
+//   "eslint-plugin-node": "^11.1.0",
+//   "eslint-plugin-prettier": "^5.2.1",
+//   "eslint-plugin-react": "^7.35.0",
+//   "prettier": "^3.3.3"
--- server.js
+++ server.js
@@ -1,6 +1,52 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
+dotenv.config({ path: './config.env' });
 
-const port = 8000;
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASEPASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    // useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log('DB connection Successful');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'tq fdcfg iuojfk',
+  rating: 2.4,
+  price: 2000,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error :( ', err);
+  });
+
+// console.log(app.get('env'));
+// console.log(process.env);
+
+const port = 27017 || process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
