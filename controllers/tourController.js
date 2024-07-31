219154fb33a4b4bd8d2f420d48f67fe87e534b1a
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-07-31 10:41:37 +0545

    Advance Filtering(lt,lte,gt,gte)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require('./../models/tourModels');
+const Tour = require('../models/tourModel.js');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
@@ -6,19 +6,32 @@ const Tour = require('./../models/tourModels');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
+
     /// BUILD QUERY
+    // 1. FILTERING
+
+    // console.log(req.query, queryObj);
+    // console.log(req.query);
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // console.log(req.query, queryObj);
-    // console.log(req.query);
-    const query = Tour.find(queryObj);
+    //2. ADVANCED FILTERING
 
-    // const tour = await Tour.find().where('duration').equals(5);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    // {difficulty: 'easy',duration: {$gte: 5}}
+    // {difficulty: 'easy',duration: {gte: 5}}
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     ///EXECUTE QUERY
     const tours = await query;
+
+    // const tour = await Tour.find().where('duration').equals(5);
 
     /// SEND RESPONSE
     res.status(200).json({
