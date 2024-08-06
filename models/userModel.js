9f2c4220549ebc5805a26317f30799705dbfadc9
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-08-06 18:38:51 +0545

    LogIn and Authorization

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const { validate } = require('./tourModel');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,12 +22,39 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm a password'],
+    validate: {
+      //only works on SAVE and on CREATE!!!
+      validator: function (el) {
+        return el === this.password; // returns true if asd === asd
+      },
+      message: 'Passwords are not the same',
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  //only run if pwd is modified
+  if (!this.isModified('password')) return next();
+
+  //hash the pwd with cost 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //delete pwdconfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
