a31f427409d1d1e5772eb60e08194a3b3c66bd08
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-08-08 21:51:57 +0545

    Setting Security HTTP Headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandeler = require('./controllers/errorController');
@@ -10,10 +11,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //M  1. GLOBAL MIDDLEWARE
+//Set security http headers
+app.use(helmet());
+
+//Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -23,15 +29,15 @@ app.use('/api', limiter);
 
 app.use(morgan('dev'));
 
-app.use(express.json());
-
+//Body parser, reading data from boody into rerq.body
+app.use(express.json({ limit: '10kb' }));
 /////////////////
 // TO USE STATIC FILE FOLDERS IN OUR APPLICATION
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
+  // console.log(req.headers);
   next();
 });
 
