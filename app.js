e9b4480740fb6b9d2df90aad14157d1f498c1391
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-08-09 17:02:09 +0545

    Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandeler = require('./controllers/errorController');
@@ -31,6 +33,13 @@ app.use(morgan('dev'));
 
 //Body parser, reading data from boody into rerq.body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
 /////////////////
 // TO USE STATIC FILE FOLDERS IN OUR APPLICATION
 app.use(express.static(`${__dirname}/public`));
